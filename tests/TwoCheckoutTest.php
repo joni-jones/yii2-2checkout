9827e1c2c367f3ce47174935c2601560f22bf36b
Ievgen Sentiabov <jonik.lowkick@gmail.com>
2014-05-25 23:37:33 +0300

    Added tests for 2checkout instances

--- tests/TwoCheckoutTest.php
+++ tests/TwoCheckoutTest.php
@@ -20,11 +20,46 @@ class TwoCheckoutTest extends \PHPUnit_Framework_TestCase
         ]);
     }
 
+    /**
+     * @covers \yii\twocheckout\TwoCheckout::init()
+     */
     public function testInitialization()
     {
         $this->assertAttributeEquals('nti123mt', 'privateKey', $this->twoCheckout);
         $this->assertAttributeEquals('112233', 'sellerId', $this->twoCheckout);
         $this->assertAttributeEquals('tango', 'secretWord', $this->twoCheckout);
+    }
+
+    /**
+     * @covers \yii\twocheckout\TwoCheckout::getCharge()
+     */
+    public function testChargeInstance()
+    {
+        $this->assertInstanceOf('\yii\twocheckout\TwoCheckoutCharge', $this->twoCheckout->charge);
+    }
+
+    /**
+     * @covers \yii\twocheckout\TwoCheckout::getMessage()
+     */
+    public function testMessageInstance()
+    {
+        $this->assertInstanceOf('\yii\twocheckout\TwoCheckoutMessage', $this->twoCheckout->message);
+    }
+
+    /**
+     * @covers \yii\twocheckout\TwoCheckout::getNotification()
+     */
+    public function testNotificationInstance()
+    {
+        $this->assertInstanceOf('\yii\twocheckout\TwoCheckoutNotification', $this->twoCheckout->notification);
+    }
+
+    /**
+     * @covers \yii\twocheckout\TwoCheckout::getReturn()
+     */
+    public function testReturnInstance()
+    {
+        $this->assertInstanceOf('\yii\twocheckout\TwoCheckoutReturn', $this->twoCheckout->return);
     }
 }
  
